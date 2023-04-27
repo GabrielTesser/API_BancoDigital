2be9d0d096a0d97a5a6c6f450cc5d94bd9cb61ee
GABRIELTESSER <gabrieltesser10@gmail.com>
2023-04-27 14:03:51 -0300

    rotas

--- rotas.php
+++ rotas.php
@@ -1,18 +1,36 @@
 <?php
 
+use Api\DAO\CorrentistaDAO;
 use App\Controller\{CorrentistaController, ContaController};
 
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch ($url)
 {
+
+    // rotas correntista
+
     case '/correntista/save':
         CorrentistaController::salvar();
+    break;
+
+    case '/correntista':
+        CorrentistaController::listar();
+    break;
+
+    case '/correntista/buscar':
+        CorrentistaController::buscar();
+    break;
+
+    case '/correntista/deletar':
+        CorrentistaController::deletar();
     break;
 
     case '/conta/extrato':
         ContaController::getExtrato();
     break;
+
+    //
 
     case '/conta/pix/receber':
         ContaController::PixReceber();
@@ -21,10 +39,6 @@ switch ($url)
     case '/conta/pix/receber':
         ContaController::PixEviar();
     break;
-
-    /*case '/correntista/entrar':
-        CorrentistaController::Login();
-    break;*/
     
     default:
         http_response_code(403);
